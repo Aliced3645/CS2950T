959a2ad309fa83fb14f590abedefb49d8b8bbb44
Shu Zhang <shu_zhang1989@yahoo.com.cn>
2012-11-26 22:09:02 -0500

    avg bounds

---
+++ SqueezeDBSystem/src/Offline/TestCPLEX.java
@@ -0,0 +1,23 @@
+package Offline;
+
+import ilog.concert.*;
+import ilog.cplex.*;
+
+
+public class TestCPLEX {
+
+	/**
+	 * @param args
+	 * @throws IloException 
+	 */
+	public static void main(String[] args) throws IloException {
+		// TODO Auto-generated method stub
+		System.out.println("wocao");
+		
+		IloCplex cplex = new IloCplex();
+		
+		
+	}
+	
+}
+
--- SqueezeDBSystem/src/Online/Avg.java
+++ SqueezeDBSystem/src/Online/Avg.java
@@ -1,5 +1,10 @@
 package Online;
 
+import ilog.concert.IloException;
+import ilog.concert.IloNumExpr;
+import ilog.concert.IloNumVar;
+import ilog.cplex.IloCplex;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -8,7 +13,9 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 public class Avg {
-	public static double process(ResultSet result, int sample_size, int db_size) throws SQLException {
+
+	public static double process(ResultSet result, int sample_size, int db_size)
+			throws SQLException {
 
 		HashMap<Integer, Integer> frequencies = new HashMap<Integer, Integer>();
 		Integer k;
@@ -26,13 +33,85 @@ public class Avg {
 
 		}
 
-
-		Iterator<Map.Entry<Integer, Integer>> it = frequencies.entrySet().iterator();
+		Iterator<Map.Entry<Integer, Integer>> it = frequencies.entrySet()
+				.iterator();
 		while (it.hasNext()) {
 			Entry<Integer, Integer> entry = (Entry<Integer, Integer>) it.next();
-			avg += entry.getKey() * (entry.getValue() / (double)sample_size);
+			avg += entry.getKey() * (entry.getValue() / (double) sample_size);
 		}
 
 		return avg;
+	}
+
+	public static double[] calculateAvgConfidenceInterval(ResultSet resultSet,
+			int sampleSize, int dbSize) {
+		double[] bounds = new double[2];
+		CplexSolution solution_min;
+		CplexSolution solution_max;
+
+		HashMap<Integer, Integer> frequencies = new HashMap<Integer, Integer>();
+		double[] selectivity;
+
+		double eta = (double) dbSize / ((double) sampleSize);
+		double query_selectivity = 0;
+
+		Integer k;
+		Integer v;
+
+		int min = 10000, max = 0;
+
+		try {
+			while (resultSet.next()) {
+				k = new Integer(resultSet.getInt("value"));
+
+				// update max and min
+				if (k.intValue() > max)
+					max = k.intValue();
+				else if (k.intValue() < min)
+					min = k.intValue();
+				
+				if (frequencies.containsKey(k)) // already in the list
+				{
+					v = frequencies.get(k);
+					v += 1;
+					frequencies.put(k, new Integer(v));
+				} else {
+					frequencies.put(k, new Integer(1));
+				}
+			}
+			
+			selectivity = new double[max - min + 1];
+			
+			for (int i = min, index = 0; i <= max; i++, index++) {
+				v = frequencies.get(new Integer(i));
+
+				if (v != null) {
+					selectivity[index] = v.intValue() / ((double) sampleSize);
+				} else {
+					selectivity[index] = 0;
+				}
+			}
+			// get query selectivity ( sum of selectivity)
+			for (int i = 0; i < (max - min + 1); i++) {
+				query_selectivity += selectivity[i];
+			}
+			
+			// min bound
+			solution_min = SumSolver.sumSolver(min, max, query_selectivity,
+						selectivity, eta, .02, false);
+			// max bound
+			solution_max = SumSolver.sumSolver(min, max, query_selectivity,
+						selectivity, eta, .02, true);
+			
+			bounds[0] = (solution_min.objective_value * dbSize) / ( dbSize * ( query_selectivity - 0.02));
+			bounds[1] = solution_max.objective_value * dbSize / ( dbSize * ( query_selectivity - 0.02));
+			
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+			// System.out.println(e.getMessage());
+		}
+
+		return bounds;
+		
 	}
 }
